52822e3eefafc532d02cf22c45ea3063a118056d
kantuni <henrikh.kantuni@gmail.com>
2019-05-10 12:10:51 -0400

    A much cleaner solution

--- 13/roman-to-integer.cpp
+++ 13/roman-to-integer.cpp
@@ -1,28 +1,26 @@
 class Solution {
   public:
     int romanToInt(string s) {
-      map<string, int> memo = {
-        {"I", 1}, {"IV", 4},
-        {"V", 5}, {"IX", 9},
-        {"X", 10}, {"XL", 40},
-        {"L", 50}, {"XC", 90},
-        {"C", 100}, {"CD", 400},
-        {"D", 500}, {"CM", 900},
-        {"M", 1000}
+      map<char, int> memo = {
+        {'I', 1},
+        {'V', 5},
+        {'X', 10},
+        {'L', 50},
+        {'C', 100},
+        {'D', 500},
+        {'M', 1000}
       };
       int ans = 0, i;
       for (i = 0; i < s.size() - 1; i++) {
-        string cur = string(1, s[i]);
-        string nxt = string(1, s[i + 1]);
-        if (memo[nxt] > memo[cur]) {
-          ans += memo[cur + nxt];
+        if (memo[s[i + 1]] > memo[s[i]]) {
+          ans += memo[s[i + 1]] - memo[s[i]];
           i++;
         } else {
-          ans += memo[cur];
+          ans += memo[s[i]];
         }
       }
       if (i == s.size() - 1) {
-        ans += memo[string(1, s[i])];
+        ans += memo[s[i]];
       }
       return ans;
     }
