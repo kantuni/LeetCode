f1de974ab7aa703eec2368f364e2125f54cabaaa
kantuni <henrikh.kantuni@gmail.com>
2019-05-10 10:51:20 -0400

    Update two-sum.cpp

--- 1/two-sum.cpp
+++ 1/two-sum.cpp
@@ -1,23 +1,14 @@
 class Solution {
   public:
     vector<int> twoSum(vector<int>& nums, int target) {
-      vector<pair<int, int>> nis;
+      map<int, int> memo;
       for (int i = 0; i < nums.size(); i++) {
-        nis.push_back({nums[i], i});
-      }
-      sort(nis.begin(), nis.end());
-      vector<int> ans;
-      for (auto it = nis.begin(); it != nis.end(); it++) {
-        auto [n1, i1] = *it;
-        auto key = make_pair(target - n1, 0);
-        auto lower = lower_bound(it + 1, nis.end(), key);
-        if (lower != nis.end()) {
-          auto [n2, i2] = *lower;
-          if (n2 == target - n1) {
-            ans = {i1, i2};
-          }
+        int cmpl = target - nums[i];
+        if (memo.count(cmpl) and memo[cmpl] != i) {
+          return {i, memo[cmpl]};
         }
+        memo[nums[i]] = i;
       }
-      return ans;
+      return {};
     }
 };
