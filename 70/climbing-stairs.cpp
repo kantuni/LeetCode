627e24746d9f32936a7fe9f1990f103625e303c2
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-05-13 15:44:28 -0400

    Update climbing-stairs.cpp

--- 70/climbing-stairs.cpp
+++ 70/climbing-stairs.cpp
@@ -1,7 +1,7 @@
 class Solution {
   public:
     int climbStairs(int n) {
-      int a = 0, b = 1, c;
+      int a = 0, b = 1, c = 1;
       for (int i = 0; i < n; i++) {
         c = a + b;
         a = b, b = c;
