70d5e404aecf7a6ec785a0fe216816831eda29ff
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-06-03 23:49:26 -0400

    Update partition-equal-subset-sum.cpp

--- 416/partition-equal-subset-sum.cpp
+++ 416/partition-equal-subset-sum.cpp
@@ -1,24 +1,18 @@
 class Solution {
   public:
     bool canPartition(vector<int>& nums) {
-      int n = nums.size();
-      int sum = accumulate(nums.begin(), nums.end(), 0);
       bool ans = false;
+      int sum = accumulate(nums.begin(), nums.end(), 0);
       if (sum % 2 == 0) {
         int target = sum / 2;
-        vector<vector<int>> memo(n + 1, vector<int>(target + 1));
-        for (int i = 0; i < n + 1; i++) {
-          memo[i][0] = 1;
-        }
-        for (int i = 1; i < n + 1; i++) {
-          for (int j = 0; j < nums[i - 1] and j < target + 1; j++) {
-            memo[i][j] = memo[i - 1][j];
-          }
-          for (int j = nums[i - 1]; j < target + 1; j++) {
-            memo[i][j] = memo[i - 1][j] || memo[i - 1][j - nums[i - 1]];
+        vector<int> memo(target + 1);
+        memo[0] = 1;
+        for (int i = 0; i < nums.size(); i++) {
+          for (int j = target; j >= nums[i]; j--) {
+            memo[j] = memo[j] || memo[j - nums[i]];
           }
         }
-        ans = memo[n][target];
+        ans = memo[target];
       }
       return ans;
     }
