723c728152b047a134350008bc36ca370dbe4499
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-04 14:54:20 -0400

    Update sort-an-array.cpp

--- 912/sort-an-array.cpp
+++ 912/sort-an-array.cpp
@@ -1,20 +1,16 @@
 class Solution {
   public:
     vector<int> sortArray(vector<int>& nums) {
-      const int N = nums.size();
       const int M = 100001;
       vector<int> freq(M);
       for (auto num: nums) {
         freq[num + M / 2]++;
       }
-      vector<int> sorted(N);
       for (int i = 0, j = 0; i < freq.size(); i++) {
-        while (freq[i] > 0) {
-          sorted[j] = i - M / 2;
-          freq[i]--;
-          j++;
+        while (freq[i]--) {
+          nums[j++] = i - M / 2;
         }
       }
-      return sorted;
+      return nums;
     }
 };
