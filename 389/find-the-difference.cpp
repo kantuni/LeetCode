198fe3cfba0320b20dca7693aacaefd4b1bdd04f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-07 14:50:03 -0400

    Update find-the-difference.cpp

--- 389/find-the-difference.cpp
+++ 389/find-the-difference.cpp
@@ -1,20 +1,12 @@
 class Solution {
   public:
     char findTheDifference(string s, string t) {
-      vector<int> ms(26);
+      int ans = 0;
       for (auto c: s) {
-        ms[c - 'a']++;
+        ans = ans xor c;
       }
-      vector<int> mt(26);
       for (auto c: t) {
-        mt[c - 'a']++;
-      }
-      char ans;
-      for (int i = 0; i < 26; i++) {
-        if (ms[i] != mt[i]) {
-          ans = i + 'a';
-          break;
-        }
+        ans = ans xor c;
       }
       return ans;
     }
