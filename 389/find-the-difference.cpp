055aa2934b257edad0cfa72fbfc4badbdd1c2808
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-07 14:50:31 -0400

    Update find-the-difference.cpp

--- 389/find-the-difference.cpp
+++ 389/find-the-difference.cpp
@@ -2,11 +2,11 @@ class Solution {
   public:
     char findTheDifference(string s, string t) {
       int ans = 0;
-      for (auto c: s) {
-        ans = ans xor c;
+      for (auto si: s) {
+        ans = ans xor si;
       }
-      for (auto c: t) {
-        ans = ans xor c;
+      for (auto ti: t) {
+        ans = ans xor ti;
       }
       return ans;
     }
