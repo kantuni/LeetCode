23c76c9c0cb3f2b8b613949c15c0a4c6e3254f1b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-12 07:25:52 -0400

    Update pascals-triangle.cpp

--- 118/pascals-triangle.cpp
+++ 118/pascals-triangle.cpp
@@ -11,11 +11,8 @@ class Solution {
       for (int i = 2; i < numRows; i++) {
         vector<int> nextRow;
         nextRow.push_back(1);
-        for (int j = 0; j < ans.back().size() - 1; j++) {
-          nextRow.push_back(
-            ans[ans.size() - 1][j] + 
-            ans[ans.size() - 1][j + 1]
-          );
+        for (int j = 0; j < i - 1; j++) {
+          nextRow.push_back(ans[i - 1][j] + ans[i - 1][j + 1]);
         }
         nextRow.push_back(1);
         ans.push_back(nextRow);
