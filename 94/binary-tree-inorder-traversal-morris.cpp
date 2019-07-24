341301525574417337f786383670a4682867cdab
kantuni <henrikh.kantuni@gmail.com>
2019-07-24 00:32:55 -0400

    Binary Tree Inorder Traversal - Morris Traversal

---
+++ 94/binary-tree-inorder-traversal-morris.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *   int val;
+ *   TreeNode *left;
+ *   TreeNode *right;
+ *   TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+  public:
+    vector<int> inorderTraversal(TreeNode* root) {
+      vector<int> ans;
+      auto cur = root;
+      while (cur != nullptr) {
+        if (cur->left != nullptr) {
+          auto pred = cur->left;
+          while (pred->right != nullptr) {
+            pred = pred->right;
+          }
+          auto tmp = cur;
+          cur = cur->left;
+          tmp->left = nullptr;
+          pred->right = tmp;
+        } else {
+          ans.push_back(cur->val);
+          cur = cur->right;
+        }
+      }
+      return ans;
+    }
+};
