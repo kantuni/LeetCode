6cdfee63ff8b0e16b464e1f9527f91d739f88f89
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-07-25 12:22:37 -0400

    Update binary-tree-level-order-traversal.cpp

--- 102/binary-tree-level-order-traversal.cpp
+++ 102/binary-tree-level-order-traversal.cpp
@@ -8,34 +8,28 @@
  * };
  */
 class Solution {
-  private:
-    int countLevels(TreeNode* root) {
-      if (root == nullptr) {
-        return 0;
-      }
-      return 1 + max(countLevels(root->left), countLevels(root->right));
-    }
-
   public:
     vector<vector<int>> levelOrder(TreeNode* root) {
       if (root == nullptr) {
         return {};
       }
-      int lvls = countLevels(root);
-      vector<vector<int>> ans(lvls);
-      ans[0].push_back(root->val);
-      queue<pair<TreeNode*, int>> q;
-      q.push({root, 0});
+      vector<vector<int>> ans;
+      queue<TreeNode*> q;
+      q.push(root);
       while (!q.empty()) {
-        auto [cur, lvl] = q.front(); q.pop();
-        if (cur->left != nullptr) {
-          q.push({cur->left, lvl + 1});
-          ans[lvl + 1].push_back(cur->left->val);
+        vector<int> lvl;
+        int cnt = q.size();
+        while (cnt--) {
+          auto cur = q.front(); q.pop();
+          lvl.push_back(cur->val);
+          if (cur->left != nullptr) {
+            q.push(cur->left);
+          }
+          if (cur->right != nullptr) {
+            q.push(cur->right);
+          }
         }
-        if (cur->right != nullptr) {
-          q.push({cur->right, lvl + 1});
-          ans[lvl + 1].push_back(cur->right->val);
-        }
+        ans.push_back(lvl);
       }
       return ans;
     }
