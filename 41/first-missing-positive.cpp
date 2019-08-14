6cffaa76b4653b67eacbda24640f3e007adc323d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-08-14 14:21:44 -0400

    Update first-missing-positive.cpp

--- 41/first-missing-positive.cpp
+++ 41/first-missing-positive.cpp
@@ -1,16 +1,19 @@
 class Solution {
   public:
     int firstMissingPositive(vector<int>& nums) {
-      for (int i = nums.size() - 1; i > -1; i--) {
+      for (int i = 0; i < nums.size(); i++) {
+        // Put 1 in the 0th place, 2 in the 1st place, etc.
+        // Don't touch numbers that are less than 1 or 
+        // are greater than the length of the array.
         bool in = nums[i] > 0 and nums[i] - 1 < nums.size();
         if (in and nums[i] != nums[nums[i] - 1]) {
           swap(nums[i], nums[nums[i] - 1]);
-          i++;
+          i--;
         }
       }
       int ans = 1;
-      for (int i = 0; i < nums.size(); i++) {
-        if (nums[i] != ans) {
+      for (auto num: nums) {
+        if (num != ans) {
           break;
         }
         ans++;
