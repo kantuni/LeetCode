d47083eb6b1e4ec115acdf5f1813e92eef1cd16e
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-02 04:35:38 -0400

    Update string-without-aaa-or-bbb.cpp

--- 984/string-without-aaa-or-bbb.cpp
+++ 984/string-without-aaa-or-bbb.cpp
@@ -3,8 +3,8 @@ class Solution {
     string strWithout3a3b(int A, int B) {
       string s;
       while (A > 0 or B > 0) {
-        bool aa = s.size() > 1 and s[s.size() - 1] == s[s.size() - 2] and s[s.size() - 1] == 'a';
-        bool bb = s.size() > 1 and s[s.size() - 1] == s[s.size() - 2] and s[s.size() - 1] == 'b';
+        bool aa = s.size() > 1 and s[s.size() - 1] == 'a' and s[s.size() - 1] == s[s.size() - 2];
+        bool bb = s.size() > 1 and s[s.size() - 1] == 'b' and s[s.size() - 1] == s[s.size() - 2];
         if (aa) {
           s += "b";
           B--;
