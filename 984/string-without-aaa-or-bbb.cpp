4cece777024b2e988651f87e6efbedcdb5968ba9
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-09-02 04:33:20 -0400

    Update string-without-aaa-or-bbb.cpp

--- 984/string-without-aaa-or-bbb.cpp
+++ 984/string-without-aaa-or-bbb.cpp
@@ -1,28 +1,23 @@
 class Solution {
   public:
     string strWithout3a3b(int A, int B) {
-      bool flip = false;
-      if (B > A) {
-        flip = true;
-        swap(A, B);
-      }
       string s;
-      while (A > 1 and B > 0 and A - B > 1) {
-        s += "aab";
-        A -= 2, B--;
-      }
-      while (A > 0 and B > 0) {
-        s += "ab";
-        A--, B--;
-      }
-      while (A--) {
-        s += "a";
-      }
-      while (B--) {
-        s += "b";
-      }
-      for (int i = 0; flip and i < s.size(); i++) {
-        s[i] = s[i] == 'b' ? 'a' : 'b';
+      while (A > 0 || B > 0) {
+        bool aa = s.size() > 1 and s[s.size() - 1] == s[s.size() - 2] and s[s.size() - 1] == 'a';
+        bool bb = s.size() > 1 and s[s.size() - 1] == s[s.size() - 2] and s[s.size() - 1] == 'b';
+        if (aa) {
+          s += "b";
+          B--;
+        } else if (bb) {
+          s += "a";
+          A--;
+        } else if (A > 0 and A > B) {
+          s += "a";
+          A--;
+        } else {
+          s += "b";
+          B--;
+        }
       }
       return s;
     }
