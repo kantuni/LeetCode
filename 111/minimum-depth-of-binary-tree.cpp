3d0640c4b68b6d2ceed7af1b1a73a97144efbdcc
kantuni <henrikh.kantuni@gmail.com>
2019-11-02 12:54:16 +0400

    Elegant solution

--- 111/minimum-depth-of-binary-tree.cpp
+++ 111/minimum-depth-of-binary-tree.cpp
@@ -14,18 +14,8 @@ class Solution {
       if (root == nullptr) {
         return 0;
       }
-      if (root->left == nullptr and root->right == nullptr) {
-        return 1;
-      }
-      int depth = INT_MAX;
-      if (root->left != nullptr) {
-        int ld = minDepth(root->left);
-        depth = min(depth, ld);
-      }
-      if (root->right != nullptr) {
-        int rd = minDepth(root->right);
-        depth = min(depth, rd);
-      }
-      return 1 + depth;
+      int ld = minDepth(root->left);
+      int rd = minDepth(root->right);
+      return (ld == 0 or rd == 0) ? ld + rd + 1 : min(ld, rd) + 1;
     }
 };
