766889e975c79efd446c1fd86569d2052c6b7b4f
kantuni <henrikh.kantuni@gmail.com>
2019-11-03 15:53:02 +0400

    Memory optimization

--- 119/pascals-triangle-ii.cpp
+++ 119/pascals-triangle-ii.cpp
@@ -1,15 +1,12 @@
 class Solution {
   public:
     vector<int> getRow(int rowIndex) {
-      vector<int> row = {1};
+      vector<int> row(rowIndex + 1);
+      row[0] = 1;
       while (rowIndex--) {
-        vector<int> tmp(row.size() + 1);
-        tmp[0] = 1;
-        for (int i = 1; i < row.size(); i++) {
-          tmp[i] = row[i - 1] + row[i];
+        for (int i = row.size() - 1; i > 0; i--) {
+          row[i] += row[i - 1];
         }
-        tmp[tmp.size() - 1] = 1;
-        row = tmp;
       }
       return row;
     }
