f43650e29a3170ead337048c1db456b989c714cb
kantuni <henrikh.kantuni@gmail.com>
2019-11-08 02:31:06 +0400

    An elegant solution

--- 160/intersection-of-two-linked-lists.cpp
+++ 160/intersection-of-two-linked-lists.cpp
@@ -8,18 +8,34 @@
  */
 
 class Solution {
+  private:
+    int getLength(ListNode* head) {
+      int len = 0;
+      while (head != nullptr) {
+        head = head->next;
+        len++;
+      }
+      return len;
+    }
+
   public:
     ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
-      ListNode* ha = headA;
-      while (ha != nullptr) {
-        ListNode* hb = headB;
-        while (hb != nullptr) {
-          if (ha == hb) {
-            return ha;
+      int alen = getLength(headA);
+      int blen = getLength(headB);
+      while (alen > 0 and blen > 0) {
+        if (alen > blen) {
+          headA = headA->next;
+          alen--;
+        } else if (alen < blen) {
+          headB = headB->next;
+          blen--;
+        } else {
+          if (headA == headB) {
+            return headA;
           }
-          hb = hb->next;
+          headA = headA->next;
+          headB = headB->next;
         }
-        ha = ha->next;
       }
       return nullptr;
     }
