c6790ef766719915215d945139dfd96ac3f65cab
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-11-08 16:38:00 +0400

    Update excel-sheet-column-title.cpp

--- 168/excel-sheet-column-title.cpp
+++ 168/excel-sheet-column-title.cpp
@@ -1,14 +1,10 @@
 class Solution {
-  private:
-    string helper(int n) {
-      if (n < 26) {
-        return string(1, n + 'A');
-      }
-      return helper(n / 26 - 1) + helper(n % 26);
-    }
-
   public:
     string convertToTitle(int n) {
-      return helper(n - 1);
+      if (n == 0) {
+        return "";
+      }
+      n -= 1;
+      return convertToTitle(n / 26) + string(1, 'A' + (n % 26));
     }
 };
