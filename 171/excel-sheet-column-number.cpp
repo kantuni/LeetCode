74261fe14a90ddcc34ec74dcb31877e512d8b450
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-11-08 16:46:25 +0400

    Update excel-sheet-column-number.cpp

--- 171/excel-sheet-column-number.cpp
+++ 171/excel-sheet-column-number.cpp
@@ -3,8 +3,7 @@ class Solution {
     int titleToNumber(string s) {
       int ans = 0;
       for (int i = 0; i < s.size(); i++) {
-        ans *= 26;
-        ans += s[i] - 'A' + 1;
+        ans = ans * 26 + (s[i] - 'A' + 1);
       }
       return ans;
     }
