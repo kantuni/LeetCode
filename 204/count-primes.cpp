32e552165a4879adacbd2ef790e691740364f1b5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-11-13 01:30:53 +0400

    Update count-primes.cpp

--- 204/count-primes.cpp
+++ 204/count-primes.cpp
@@ -4,6 +4,7 @@ class Solution {
       vector<bool> isPrime(n + 1, true);
       isPrime[0] = isPrime[1] = false;
       for (int i = 2; i * i <= n; i++) {
+        if (!isPrime[i]) continue;
         for (int j = i * i; j <= n; j += i) {
           isPrime[j] = false;
         }
