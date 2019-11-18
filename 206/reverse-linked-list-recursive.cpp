f2276873d6b104ec3cd882904e8777b283269cf7
kantuni <henrikh.kantuni@gmail.com>
2019-11-19 03:08:06 +0400

    Reverse Linked List - Recursive

---
+++ 206/reverse-linked-list-recursive.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *   int val;
+ *   ListNode *next;
+ *   ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+
+class Solution {
+  public:
+    ListNode* reverseList(ListNode* head) {
+      if (head == nullptr) {
+        return nullptr;
+      }
+      if (head->next == nullptr) {
+        return head;
+      }
+      ListNode *newHead = reverseList(head->next);
+      ListNode *tmp = newHead;
+      while (tmp->next != nullptr) {
+        tmp = tmp->next;
+      }
+      tmp->next = new ListNode(head->val);
+      return newHead;
+    }
+};
