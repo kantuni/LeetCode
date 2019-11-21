6cdc66cf9cd76e770922009fdc56a7f780df08ff
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2019-11-22 00:54:55 +0400

    Update contains-duplicate-ii.cpp

--- 219/contains-duplicate-ii.cpp
+++ 219/contains-duplicate-ii.cpp
@@ -8,7 +8,7 @@ class Solution {
       }
       sort(numsp.begin(), numsp.end());
       for (int i = 0; i < n - 1; i++) {
-        bool same = numsp[i].first == numsp[i + 1].first;
+        bool same = numsp[i + 1].first == numsp[i].first;
         int dist = numsp[i + 1].second - numsp[i].second;
         if (same and dist <= k) {
           return true;
