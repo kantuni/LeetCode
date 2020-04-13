030054b865f9c5e83d7d15683da902fdee4e7cdc
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-13 14:36:07 +0400

    Update contiguous-array.cpp

--- unknown/contiguous-array.cpp
+++ unknown/contiguous-array.cpp
@@ -1,26 +1,17 @@
 class Solution {
   public:
     int findMaxLength(vector<int>& nums) {
-      int n = nums.size();
-      for (int i = 0; i < n; i++) {
-        nums[i] = (nums[i] == 0) ? -1 : nums[i];
-      }
-      vector<int> ps(n + 1);
-      for (int i = 1; i < ps.size(); i++) {
-        ps[i] = ps[i - 1] + nums[i - 1];
-      }
       map<int, vector<int>> memo;
-      for (int i = 1; i < ps.size(); i++) {
-        memo[ps[i]].push_back(i);
+      int cnt = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        cnt += (nums[i] == 1) ? 1 : -1;
+        memo[cnt].push_back(i);
       }
       int ans = 0;
-      for (int i = 1; i < ps.size(); i++) {
-        if (ps[i] == 0) {
-          ans = max(ans, i);
-        }
-      }
       for (auto [key, values]: memo) {
-        int len = values.back() - values.front();
+        int len = (key == 0)
+            ? values.back() + 1
+            : values.back() - values.front();
         ans = max(ans, len);
       }
       return ans;
