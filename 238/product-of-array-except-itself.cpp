a3b542d13bebfa592b81bd9fbe485cbf1aa21e78
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-15 14:10:10 +0400

    Update product-of-array-except-itself.cpp

--- 238/product-of-array-except-itself.cpp
+++ 238/product-of-array-except-itself.cpp
@@ -2,17 +2,14 @@ class Solution {
   public:
     vector<int> productExceptSelf(vector<int>& nums) {
       int n = nums.size();
-      vector<int> pp(n + 1, 1);
-      for (int i = 1; i < n; i++) {
-        pp[i] = pp[i - 1] * nums[i - 1];
+      vector<int> ans(n, 1);
+      for (int i = 1, p = 1; i < n; i++) {
+        ans[i] *= nums[i - 1] * p;
+        p *= nums[i - 1];
       }
-      vector<int> sp(n + 1, 1);
-      for (int i = n - 1; i > 0; i--) {
-        sp[i - 1] = sp[i] * nums[i];
-      }
-      vector<int> ans(n);
-      for (int i = 0; i < n; i++) {
-        ans[i] = pp[i] * sp[i];
+      for (int i = n - 1, p = 1; i > 0; i--) {
+        ans[i - 1] *= nums[i] * p;
+        p *= nums[i];
       }
       return ans;
     }
