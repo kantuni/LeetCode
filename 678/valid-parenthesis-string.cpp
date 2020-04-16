2e8963185c85cb3845d48359d11f22bd9580e42c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-16 14:52:47 +0400

    Update valid-parenthesis-string.cpp

--- 678/valid-parenthesis-string.cpp
+++ 678/valid-parenthesis-string.cpp
@@ -1,27 +1,22 @@
 class Solution {
-  private:
-    bool solve(string &s, int pos, int cnt) {
-      if (pos == s.size()) {
-        return cnt == 0;
-      }
-      if (cnt < 0) {
-        return false;
-      }
-      if (s[pos] == '(') {
-        return solve(s, pos + 1, cnt + 1);
-      }
-      if (s[pos] == ')') {
-        return solve(s, pos + 1, cnt - 1);
-      }
-      return (
-        solve(s, pos + 1, cnt + 1) or
-        solve(s, pos + 1, cnt - 1) or
-        solve(s, pos + 1, cnt)
-      );
-    }
-
   public:
     bool checkValidString(string s) {
-      return solve(s, 0, 0);
+      int low = 0, high = 0;
+      for (int i = 0; i < s.size(); i++) {
+        if (s[i] == '(') {
+          low++, high++;
+        } else if (s[i] == ')') {
+          low--, high--;
+        } else {
+          low--, high++;
+        }
+        // cannot be balanced anymore
+        if (high < 0) {
+          return false;
+        }
+        // ignore negative values
+        low = max(low, 0);
+      }
+      return low == 0;
     }
 };
