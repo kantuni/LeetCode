fd53cfc0764df026bfb323f1b36059411675fe6d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-17 15:30:55 +0400

    Create number-of-islands.cpp

---
+++ 200/number-of-islands.cpp
@@ -0,0 +1,32 @@
+class Solution {
+  private:
+    int dr[4] = {-1, 0, 1, 0};
+    int dc[4] = {0, -1, 0, 1};
+
+    void dfs(vector<vector<char>>& grid, int r, int c) {
+      grid[r][c] = '2';
+      for (int i = 0; i < 4; i++) {
+        int nr = r + dr[i];
+        int nc = c + dc[i];
+        bool rok = 0 <= nr and nr < grid.size();
+        bool cok = 0 <= nc and nc < grid[0].size();
+        if (rok and cok and grid[nr][nc] == '1') {
+          dfs(grid, nr, nc);
+        }
+      }
+    }
+
+  public:
+    int numIslands(vector<vector<char>>& grid) {
+      int ans = 0;
+      for (int r = 0; r < grid.size(); r++) {
+        for (int c = 0; c < grid[r].size(); c++) {
+          if (grid[r][c] == '1') {
+            dfs(grid, r, c);
+            ans++;
+          }
+        }
+      }
+      return ans;
+    }
+};
