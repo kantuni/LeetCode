6bccefbe31d7d113ffbbfbc57953e1cc7df0a6e0
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-17 15:43:03 +0400

    Update number-of-islands.cpp

--- 200/number-of-islands.cpp
+++ 200/number-of-islands.cpp
@@ -1,9 +1,9 @@
 class Solution {
   private:
     void dfs(vector<vector<char>>& grid, int r, int c) {
-      bool rok = r < 0 or r >= grid.size();
-      bool cok = c < 0 or c >= grid[0].size();
-      if (rok or cok or grid[r][c] != '1') {
+      bool rout = r < 0 or r >= grid.size();
+      bool cout = c < 0 or c >= grid[0].size();
+      if (rout or cout or grid[r][c] != '1') {
         return;
       }
       grid[r][c] = '+';
