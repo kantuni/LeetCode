d0831b2414d03e6ea3197d3bcb44dd79009b68af
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-18 12:40:57 +0400

    Update minimum-path-sum.cpp

--- 64/minimum-path-sum.cpp
+++ 64/minimum-path-sum.cpp
@@ -1,32 +1,28 @@
 class Solution {
-  private:
-    const int INF = 1e9;
-    map<int, map<int, int>> memo;
-  
-    int remember(vector<vector<int>>& grid, int r, int c) {
-      if (memo.count(r) == 0 or memo[r].count(c) == 0) {
-        memo[r][c] = solve(grid, r, c);
-      }
-      return memo[r][c];
-    }
-
-    int solve(vector<vector<int>>& grid, int r, int c) {
-      if (r >= grid.size() or c >= grid[0].size()) {
-        return INF;
-      }
-      if (r == grid.size() - 1 and c == grid[0].size() - 1) {
-        return grid[r][c];
-      }
-      int down = remember(grid, r + 1, c);
-      int right = remember(grid, r, c + 1);
-      return grid[r][c] + min(down, right);
-    }
-
   public:
     int minPathSum(vector<vector<int>>& grid) {
       if (grid.size() == 0) {
         return 0;
       }
-      return solve(grid, 0, 0);
+      int n = grid.size();
+      int m = grid[0].size();
+      int cost[n][m];
+      for (int r = 0; r < n; r++) {
+        for (int c = 0; c < m; c++) {
+          if (r == 0 and c == 0) {
+            cost[r][c] = grid[r][c];
+          } else if (r == 0 and c > 0) {
+            cost[r][c] = grid[r][c] + cost[r][c - 1];
+          } else if (r > 0 and c == 0) {
+            cost[r][c] = grid[r][c] + cost[r - 1][c];
+          } else {
+            cost[r][c] = (
+              grid[r][c] +
+              min(cost[r - 1][c], cost[r][c - 1])
+            );
+          }
+        }
+      }
+      return cost[n - 1][m - 1];
     }
 };
