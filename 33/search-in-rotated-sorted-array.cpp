dbae5040ab87a6f3e606f682ba99f8747b2da2a5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-19 20:08:31 +0400

    Update search-in-rotated-sorted-array.cpp

--- 33/search-in-rotated-sorted-array.cpp
+++ 33/search-in-rotated-sorted-array.cpp
@@ -1,37 +1,34 @@
 class Solution {
   private:
-    int findPivot(vector<int> &nums) {
-      int pivot = 0;
+    int findPivotIndex(vector<int>& nums) {
       int l = 0, r = nums.size() - 1;
       while (l <= r) {
         int m = l + (r - l) / 2;
-        if (nums[m] < nums[0]) {
-          pivot = m;
-          r = m - 1;
-        } else {
+        if (nums[0] <= nums[m]) {
           l = m + 1;
+        } else {
+          r = m - 1;
         }
       }
-      return pivot;
+      return l;
     }
 
   public:
     int search(vector<int>& nums, int target) {
-      int p = findPivot(nums);
-      int ans = -1;
+      int p = findPivotIndex(nums);
       int l = 0, r = nums.size() - 1;
       while (l <= r) {
         int m = l + (r - l) / 2;
         int mp = (m + p) % nums.size();
         if (nums[mp] == target) {
-          ans = mp;
-          break;
-        } else if (nums[mp] < target) {
+          return mp;
+        }
+        if (nums[mp] < target) {
           l = m + 1;
         } else {
           r = m - 1;
         }
       }
-      return ans;
+      return -1;
     }
 };
