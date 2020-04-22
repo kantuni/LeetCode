26e8b382536d91e3c66d60422ca2e545facf9970
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-23 01:21:39 +0400

    Create subarray-sum-equals-k.cpp

---
+++ 560/subarray-sum-equals-k.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int subarraySum(vector<int>& nums, int k) {
+      int n = nums.size();
+      vector<int> ps(n + 1);
+      for (int i = 1; i < n + 1; i++) {
+        ps[i] = ps[i - 1] + nums[i - 1];
+      }
+      map<int, int> memo;
+      for (int i = 1; i < n + 1; i++) {
+        memo[ps[i]] += 1;
+      }
+      int ans = 0;
+      for (int i = 1; i < n + 1; i++) {
+        for (int j = i + 1; j < n + 1; j++) {
+          if (ps[j] - ps[i] == k) {
+            ans++;
+          }
+        }
+      }
+      for (int i = 1; i < n + 1; i++) {
+        if (ps[i] == k) {
+          ans++;
+        }
+      }
+      return ans;
+    }
+};
