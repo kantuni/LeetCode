785a4e891543416e7abd87ba01ef6aaab9f39f7c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-04-26 03:00:22 +0400

    Update jump-game.cpp

--- 55/jump-game.cpp
+++ 55/jump-game.cpp
@@ -2,18 +2,12 @@ class Solution {
   public:
     bool canJump(vector<int>& nums) {
       int n = nums.size();
-      if (n == 1) {
-        return true;
-      }
-      vector<int> can(n, 0);
       int lastTrue = n - 1;
       for (int i = n - 2; i > -1; i--) {
-        bool nxt = nums[i] > 0 and can[i + 1];
-        bool skip = i + nums[i] >= lastTrue;
-        bool end = i + nums[i] >= n - 1;
-        can[i] = nxt or skip or end;
-        lastTrue = can[i] ? i : lastTrue;
+        if (i + nums[i] >= lastTrue) {
+          lastTrue = i;
+        }
       }
-      return can[0];
+      return lastTrue == 0;
     }
 };
