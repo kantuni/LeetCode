30bcb9e2563c269a5198452bfcbc91661ed30bb4
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-04 22:40:19 +0400

    Update bulls-and-cows.cpp

--- 299/bulls-and-cows.cpp
+++ 299/bulls-and-cows.cpp
@@ -1,21 +1,21 @@
 class Solution {
   public:
     string getHint(string secret, string guess) {
-      map<char, int> sm;
+      vector<int> sm(10);
       for (int i = 0; i < secret.size(); i++) {
-        sm[secret[i]]++;
+        sm[secret[i] - '0']++;
       }
-      map<char, int> gm;
+      vector<int> gm(10);
       for (int i = 0; i < guess.size(); i++) {
-        gm[guess[i]]++;
+        gm[guess[i] - '0']++;
       }
       int bulls = 0;
       for (int i = 0; i < guess.size(); i++) {
         bulls += i < secret.size() and guess[i] == secret[i];
       }
       int cows = 0;
-      for (auto [c, cnt]: gm) {
-        cows += min(sm[c], cnt);
+      for (int i = 0; i < 10; i++) {
+        cows += min(sm[i], gm[i]);
       }
       return to_string(bulls) + "A" + to_string(cows - bulls) + "B";
     }
