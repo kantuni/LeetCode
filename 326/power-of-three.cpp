fa194a9501891c6df5d158d06f8ebce9eebf55ff
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-05 00:30:48 +0400

    Update power-of-three.cpp

--- 326/power-of-three.cpp
+++ 326/power-of-three.cpp
@@ -1,15 +1,12 @@
 class Solution {
   public:
     bool isPowerOfThree(int n) {
-      if (n == 0) {
+      if (n < 1) {
         return false;
       }
-      while (n != 1) {
-        if (n % 3 != 0) {
-          return false;
-        }
+      while (n % 3 == 0) {
         n /= 3;
       }
-      return true;
+      return n == 1;
     }
 };
