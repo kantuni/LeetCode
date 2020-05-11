95adfb7173abb919de0ab11313bb8390c9213d1d
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-11 23:23:55 +0400

    Update flood-fill.cpp

--- 733/flood-fill.cpp
+++ 733/flood-fill.cpp
@@ -1,17 +1,11 @@
 class Solution {
   private:
-    vector<vector<int>> color;
-
     void dfs(vector<vector<int>>& image, int r, int c, int oldColor, int newColor) {
       int n = image.size(), m = image[0].size();
-      if (r < 0 or r >= n or c < 0 or c >= m) {
-        return;
-      }
-      if (color[r][c] != 0 or image[r][c] != oldColor) {
+      if (r < 0 or r >= n or c < 0 or c >= m or image[r][c] != oldColor) {
         return;
       }
       image[r][c] = newColor;
-      color[r][c] = 1;
       dfs(image, r + 1, c, oldColor, newColor);
       dfs(image, r - 1, c, oldColor, newColor);
       dfs(image, r, c + 1, oldColor, newColor);
@@ -20,9 +14,9 @@ class Solution {
   
   public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newColor) {
-      int n = image.size(), m = image[0].size();
-      color.resize(n, vector<int>(m, 0));
-      dfs(image, sr, sc, image[sr][sc], newColor);
+      if (image[sr][sc] != newColor) {
+        dfs(image, sr, sc, image[sr][sc], newColor);
+      }
       return image;
     }
 };
