4eeea19b037abbd3538c258e88a0f2ab8ace2a79
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-15 00:43:59 +0400

    Update implement-trie-prefix-tree.cpp

--- 208/implement-trie-prefix-tree.cpp
+++ 208/implement-trie-prefix-tree.cpp
@@ -8,12 +8,10 @@ class Trie {
     Node* root;
 
   public:
-    // Initialize your data structure here.
     Trie() {
       root = new Node();
     }
-    
-    // Inserts a word into the trie.
+
     void insert(string word) {
       auto current = root;
       for (auto letter: word) {
@@ -25,8 +23,7 @@ class Trie {
       }
       current->end = true;
     }
-    
-    // Returns if the word is in the trie.
+
     bool search(string word) {
       auto current = root;
       for (auto letter: word) {
@@ -38,8 +35,7 @@ class Trie {
       }
       return current->end;
     }
-    
-    // Returns if there is any word in the trie that starts with the given prefix.
+
     bool startsWith(string prefix) {
       auto current = root;
       for (auto letter: prefix) {
