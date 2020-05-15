c42ddb2a8e20e6eb0d62962bc8e08a76e9f518de
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-05-16 02:05:08 +0400

    Update maximum-subarray.cpp

--- 53/maximum-subarray.cpp
+++ 53/maximum-subarray.cpp
@@ -1,11 +1,11 @@
 class Solution {
   public:
     int maxSubArray(vector<int>& nums) {
-      int ans = INT_MIN, sum = 0;
+      int global = INT_MIN, local = 0;
       for (int i = 0; i < nums.size(); i++) {
-        sum = (sum > 0) ? sum + nums[i] : nums[i];
-        ans = max(ans, sum);
+        local = max(local + nums[i], nums[i]);
+        global = max(global, local);
       }
-      return ans;
+      return global;
     }
 };
