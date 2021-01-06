6d8a418fe30dba0be02189962ae9aba3c3d208ba
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-01-06 22:16:18 +0400

    Create matrix-diagonal-sum.cpp

---
+++ 1572/matrix-diagonal-sum.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    int diagonalSum(vector<vector<int>>& mat) {
+      int n = mat.size();
+      int ans = 0;
+      for (int i = 0; i < n; i++) {
+        ans += mat[i][i];
+      }
+      for (int i = n - 1; i > -1; i--) {
+        ans += mat[n - 1 - i][i];
+      }
+      if (n % 2 == 1) {
+        ans -= mat[n - 1 - n / 2][n - 1 - n / 2];
+      }
+      return ans;
+    }
+};
