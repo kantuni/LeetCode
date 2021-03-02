ba2956eecb5fe146ef02eb124035f60a0b562d33
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-02 13:24:20 +0400

    Update set-mismatch.cpp

--- 645/set-mismatch.cpp
+++ 645/set-mismatch.cpp
@@ -1,16 +1,16 @@
 class Solution {
   public:
     vector<int> findErrorNums(vector<int>& nums) {
-      map<int, int> memo;
+      vector<int> cnt(nums.size());
       for (int i = 0; i < nums.size(); i++) {
-        memo[nums[i]]++;
+        cnt[nums[i] - 1]++;
       }
       int duplicate, missing;
-      for (int i = 1; i <= nums.size(); i++) {
-        if (memo[i] == 0) {
-          missing = i;
-        } else if (memo[i] == 2) {
-          duplicate = i;
+      for (int i = 0; i < cnt.size(); i++) {
+        if (cnt[i] == 0) {
+          missing = i + 1;
+        } else if (cnt[i] == 2) {
+          duplicate = i + 1;
         }
       }
       return {duplicate, missing};
