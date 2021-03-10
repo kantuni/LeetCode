2a7362d3dc696590fe25ee67f45cac2eab7a174b
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-10 18:36:46 +0400

    Update integer-to-roman.cpp

--- 12/integer-to-roman.cpp
+++ 12/integer-to-roman.cpp
@@ -1,14 +1,22 @@
 class Solution {
   public:
     string intToRoman(int num) {
-      vector<int> marks = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-      vector<string> romans = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+      vector<pair<int, string>> marks = {
+        {1000, "M"}, {900, "CM"}, 
+        {500, "D"}, {400, "CD"}, 
+        {100, "C"}, {90, "XC"},
+        {50, "L"}, {40, "XL"},
+        {10, "X"}, {9, "IX"},
+        {5, "V"}, {4, "IV"},
+        {1, "I"}
+      };
       string ans;
       int pos = 0;
       while (pos < marks.size()) {
-        if (marks[pos] <= num) {
-          ans += romans[pos];
-          num -= marks[pos];
+        auto [mark, roman] = marks[pos];
+        if (mark <= num) {
+          ans += roman;
+          num -= mark;
         } else {
           pos++;
         }
