af484f7fd6719b5227f00b3e2e3cc51a48a2b2f5
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-11 23:47:22 +0400

    Update coin-change.cpp

--- 322/coin-change.cpp
+++ 322/coin-change.cpp
@@ -6,11 +6,11 @@ class Solution {
       q.push({amount, 0});
       while (!q.empty()) {
         auto [amnt, step] = q.front(); q.pop();
-        if (amnt == 0) {
-          return step;
-        }
         if (amnt < 0) {
           continue;
+        }
+        if (amnt == 0) {
+          return step;
         }
         for (auto coin: coins) {
           if (visited.count(amnt - coin) == 0) {
