418a6cadd7310577b564c85f15b43814663a6698
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-11 23:28:23 +0400

    Update coin-change.cpp

--- 322/coin-change.cpp
+++ 322/coin-change.cpp
@@ -11,11 +11,11 @@ class Solution {
 
   public:
     int coinChange(vector<int>& coins, int amount) {
-      if (amount == 0) {
-        return 0;
-      }
       if (amount < 0) {
         return -1;
+      }
+      if (amount == 0) {
+        return 0;
       }
       int ans = INT_MAX;
       for (int i = 0; i < coins.size(); i++) {
