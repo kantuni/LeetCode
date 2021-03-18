8268940356ad057f335af4069f25033805611c80
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-19 00:56:41 +0400

    Update wiggle-subsequence.cpp

--- 376/wiggle-subsequence.cpp
+++ 376/wiggle-subsequence.cpp
@@ -4,16 +4,15 @@ class Solution {
       int down = 1, up = 1;
       for (int i = 1; i < nums.size(); i++) {
         if (nums[i] > nums[i - 1]) {
-          // It wiggles up, thus the element before it 
+          // The sequence wiggles up, thus the element before it 
           // must be a down position.
           up = down + 1;
         } else if (nums[i] < nums[i - 1]) {
-          // It wiggles down, thus the element before it 
+          // The sequence wiggles down, thus the element before it 
           // must be an up position.
           down = up + 1;
         }
-        // Otherwise, the values of up and down 
-        // should remain the same.
+        // Otherwise, the values of up and down should remain the same.
       }
       return max(down, up);
     }
