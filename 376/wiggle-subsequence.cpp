d0556a6f7e93d3d373afad6fc992085cebbaea48
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-19 01:10:50 +0400

    Update wiggle-subsequence.cpp

--- 376/wiggle-subsequence.cpp
+++ 376/wiggle-subsequence.cpp
@@ -1,19 +1,30 @@
 class Solution {
   public:
     int wiggleMaxLength(vector<int>& nums) {
-      int down = 1, up = 1;
-      for (int i = 1; i < nums.size(); i++) {
+      int n = nums.size();
+      // up[i] (down[i]) refers to the length of the longest wiggle subsequence
+      // obtained so far by considering the i-th element as the last element
+      // of the wiggle subsequence ending with a rising (falling) wiggle.
+      vector<int> up(n), down(n);
+      up[0] = down[0] = 1;
+      for (int i = 1; i < n; i++) {
         if (nums[i] > nums[i - 1]) {
           // The sequence wiggles up, thus the element before it 
           // must be a down position.
-          up = down + 1;
+          up[i] = down[i - 1] + 1;
+          down[i] = down[i - 1];
         } else if (nums[i] < nums[i - 1]) {
           // The sequence wiggles down, thus the element before it 
           // must be an up position.
-          down = up + 1;
+          up[i] = up[i - 1];
+          down[i] = up[i - 1] + 1;
+        } else {
+          // Otherwise, the values of up and down 
+          // should remain the same.
+          up[i] = up[i - 1];
+          down[i] = down[i - 1];
         }
-        // Otherwise, the values of up and down should remain the same.
       }
-      return max(down, up);
+      return max(up[n - 1], down[n - 1]);
     }
 };
