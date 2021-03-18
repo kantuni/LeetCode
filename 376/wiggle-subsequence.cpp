5adb3fbb1c999d5dfbf7931ed3aad4979dbd6a7a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-19 00:55:51 +0400

    Update wiggle-subsequence.cpp

--- 376/wiggle-subsequence.cpp
+++ 376/wiggle-subsequence.cpp
@@ -1,33 +1,20 @@
 class Solution {
-  private:
-    const int INF = 1e9;
-    map<int, map<int, map<bool, int>>> memo;
-
-    int remember(vector<int>& nums, int pos, int last, bool inc) {
-      if (memo.count(pos) == 0 or memo[pos].count(last) == 0 or memo[pos][last].count(inc) == 0) {
-        memo[pos][last][inc] = solve(nums, pos, last, inc);
-      }
-      return memo[pos][last][inc];
-    }
-
-    int solve(vector<int>& nums, int pos, int last, bool inc) {
-      if (pos == nums.size()) {
-        return 0;
-      }
-      int ans = remember(nums, pos + 1, last, inc);
-      if (inc and nums[pos] > last) {
-        ans = max(ans, 1 + remember(nums, pos + 1, nums[pos], false));
-      } else if (!inc and nums[pos] < last) {
-        ans = max(ans, 1 + remember(nums, pos + 1, nums[pos], true));
-      }
-      return ans;
-    }
-
   public:
     int wiggleMaxLength(vector<int>& nums) {
-      return max(
-        solve(nums, 0, -INF, true),
-        solve(nums, 0, INF, false)
-      );
+      int down = 1, up = 1;
+      for (int i = 1; i < nums.size(); i++) {
+        if (nums[i] > nums[i - 1]) {
+          // It wiggles up, thus the element before it 
+          // must be a down position.
+          up = down + 1;
+        } else if (nums[i] < nums[i - 1]) {
+          // It wiggles down, thus the element before it 
+          // must be an up position.
+          down = up + 1;
+        }
+        // Otherwise, the values of up and down 
+        // should remain the same.
+      }
+      return max(down, up);
     }
 };
