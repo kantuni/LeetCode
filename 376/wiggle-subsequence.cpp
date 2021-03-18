3a8ebe8addff6ceb70053e917fd1f4abdc0f6af1
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-19 01:11:13 +0400

    Update wiggle-subsequence.cpp

--- 376/wiggle-subsequence.cpp
+++ 376/wiggle-subsequence.cpp
@@ -19,8 +19,7 @@ class Solution {
           up[i] = up[i - 1];
           down[i] = up[i - 1] + 1;
         } else {
-          // Otherwise, the values of up and down 
-          // should remain the same.
+          // Otherwise, the values remain the same.
           up[i] = up[i - 1];
           down[i] = down[i - 1];
         }
