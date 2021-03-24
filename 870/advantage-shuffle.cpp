2d6ca163a8ef021e03f40ac631aa0c7b877cc42c
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-24 16:44:27 +0400

    Update advantage-shuffle.cpp

--- 870/advantage-shuffle.cpp
+++ 870/advantage-shuffle.cpp
@@ -1,37 +1,15 @@
 class Solution {
   public:
     vector<int> advantageCount(vector<int>& A, vector<int>& B) {
-      int n = A.size();
-      vector<pair<int, int>> Ax(n), Bx(n);
-      for (int i = 0; i < n; i++) {
-        Ax[i] = {A[i], i};
-        Bx[i] = {B[i], i};
-      }
-      sort(Ax.begin(), Ax.end());
-      sort(Bx.begin(), Bx.end());
-      map<int, int> win;
-      vector<int> lose;
-      int i = 0, j = 0;
-      while (i < n and j < n) {
-        while (i < n and Ax[i].first <= Bx[j].first) {
-          lose.push_back(Ax[i].first);
-          i++;
+      multiset<int> s(A.begin(), A.end());
+      for (int i = 0; i < B.size(); i++) {
+        auto it = s.upper_bound(B[i]);
+        if (it == s.end()) {
+          it = s.begin();
         }
-        if (i == n) {
-          break;
-        }
-        win[Bx[j].second] = Ax[i].first;
-        i++, j++;
+        A[i] = *it;
+        s.erase(it);
       }
-      vector<int> ans;
-      for (int i = 0; i < n; i++) {
-        if (win.count(i) > 0) {
-          ans.push_back(win[i]);
-        } else {
-          ans.push_back(lose.back());
-          lose.pop_back();
-        }
-      }
-      return ans;
+      return A;
     }
 };
