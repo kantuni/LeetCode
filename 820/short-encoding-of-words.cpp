9fd0d6ca86177c7635b498c28fb01d679c9bb527
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-29 23:25:27 +0400

    Update short-encoding-of-words.cpp

--- 820/short-encoding-of-words.cpp
+++ 820/short-encoding-of-words.cpp
@@ -1,36 +1,18 @@
 class Solution {
-  private:
-    static bool byLength(string& a, string& b) {
-      return a.size() > b.size();
-    }
-  
-    bool match(string a, string b) {
-      int i = a.size() - 1, j = b.size() - 1;
-      while (j > -1) {
-        if (a[i] != b[j]) {
-          return false;
-        }
-        i--, j--;
-      }
-      return true;
-    }
-
   public:
     int minimumLengthEncoding(vector<string>& words) {
-      vector<int> color(words.size());
-      sort(words.begin(), words.end(), byLength);
-      for (int i = 0; i < words.size(); i++) {
-        for (int j = i + 1; j < words.size(); j++) {
-          if (color[j] == 0 and match(words[i], words[j])) {
-            color[j] = 1;
+      set<string> s(words.begin(), words.end());
+      for (auto w: words) {
+        for (int i = 1; i < w.size(); i++) {
+          auto sx = w.substr(i);
+          if (s.count(sx) > 0) {
+            s.erase(sx);
           }
         }
       }
       int ans = 0;
-      for (int i = 0; i < color.size(); i++) {
-        if (color[i] == 0) {
-          ans += words[i].size() + 1;
-        }
+      for (auto w: s) {
+        ans += w.size() + 1;
       }
       return ans;
     }
