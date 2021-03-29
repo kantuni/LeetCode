f2001c25e06a61199fbd6ed46ba883918ce2cf84
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-29 15:19:33 +0400

    Update flip-binary-tree-to-match-preorder-traversal.cpp

--- 971/flip-binary-tree-to-match-preorder-traversal.cpp
+++ 971/flip-binary-tree-to-match-preorder-traversal.cpp
@@ -12,30 +12,27 @@
 
 class Solution {
   private:
-    void preorder(TreeNode* root, queue<int>& q, vector<int>& ans) {
+    void preorder(TreeNode* root, vector<int>& voyage, vector<int>& ans) {
       if (root == nullptr) {
         return;
       }
-      if (root->val != q.front()) {
+      if (root->val != voyage.back()) {
         ans.push_back(-1);
       }
-      q.pop();
-      if (root->left != nullptr and root->left->val != q.front()) {
+      voyage.pop_back();
+      if (root->left != nullptr and root->left->val != voyage.back()) {
         swap(root->left, root->right);
         ans.push_back(root->val);
       }
-      preorder(root->left, q, ans);
-      preorder(root->right, q, ans);
+      preorder(root->left, voyage, ans);
+      preorder(root->right, voyage, ans);
     }
 
   public:
     vector<int> flipMatchVoyage(TreeNode* root, vector<int>& voyage) {
-      queue<int> q;
-      for (auto value: voyage) {
-        q.push(value);
-      }
+      reverse(voyage.begin(), voyage.end());
       vector<int> ans;
-      preorder(root, q, ans);
+      preorder(root, voyage, ans);
       int cnt = count(ans.begin(), ans.end(), -1);
       if (cnt > 0) {
         return {-1};
