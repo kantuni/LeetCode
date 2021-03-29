6e501f25817437cae427235a4176c5f30a668b38
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-03-29 23:27:22 +0400

    Update short-encoding-of-words.cpp

--- 820/short-encoding-of-words.cpp
+++ 820/short-encoding-of-words.cpp
@@ -2,16 +2,16 @@ class Solution {
   public:
     int minimumLengthEncoding(vector<string>& words) {
       set<string> s(words.begin(), words.end());
-      for (auto w: words) {
+      for (string w: words) {
         for (int i = 1; i < w.size(); i++) {
-          auto sx = w.substr(i);
+          string sx = w.substr(i);
           if (s.count(sx) > 0) {
             s.erase(sx);
           }
         }
       }
       int ans = 0;
-      for (auto w: s) {
+      for (string w: s) {
         ans += w.size() + 1;
       }
       return ans;
