b90f053589aa196e0c9e25f81f6a7333522648f3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2021-09-14 15:55:20 +0400

    Update accounts-merge.cpp

--- 721/accounts-merge.cpp
+++ 721/accounts-merge.cpp
@@ -1,32 +1,33 @@
-class Solution {
-  private:
-    vector<int> parent;
-  
-    int find(int x) {
-      while (x != parent[x]) {
-        x = parent[x];
-      }
-      return x;
-    }
-  
-    bool same(int x, int y) {
-      return find(x) == find(y);
-    }
-  
-    void merge(int x, int y) {
-      int px = find(x);
-      int py = find(y);
-      if (rand() % 2 == 0) {
-        parent[px] = py;
-      } else {
-        parent[py] = px;
-      }
-    }
+struct UnionFind {
+  vector<int> parent;
 
+  UnionFind(int n) {
+    parent.assign(n, 0);
+    iota(parent.begin(), parent.end(), 0);
+  }
+  
+  int find(int x) {
+    return x == parent[x] ? x : parent[x] = find(parent[x]);
+  }
+
+  bool same(int x, int y) {
+    return find(x) == find(y);
+  }
+
+  void merge(int x, int y) {
+    x = find(x), y = find(y);
+    if (rand() % 2 == 0) {
+      parent[x] = y;
+    } else {
+      parent[y] = x;
+    }
+  }
+};
+
+class Solution {
   public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accounts) {
-      parent.resize(accounts.size());
-      iota(parent.begin(), parent.end(), 0);
+      UnionFind memo(accounts.size());
       map<string, int> emailToAccountIndex;
       for (int i = 0; i < accounts.size(); i++) {
         for (int j = 1; j < accounts[i].size(); j++) {
@@ -34,13 +35,13 @@ class Solution {
           if (emailToAccountIndex.count(email) == 0) {
             emailToAccountIndex[email] = i;
           } else {
-            merge(emailToAccountIndex[email], i);
+            memo.merge(emailToAccountIndex[email], i);
           }
         }
       }
       map<int, set<string>> accountIndexToEmails;
       for (auto [email, accountIndex]: emailToAccountIndex) {
-        accountIndexToEmails[find(accountIndex)].insert(email);
+        accountIndexToEmails[memo.find(accountIndex)].insert(email);
       }
       vector<vector<string>> ans;
       for (auto [accountIndex, emails]: accountIndexToEmails) {
