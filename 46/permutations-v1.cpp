7ea2cd3f84b077ece5de742373966fd7d1f1248a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-03-22 11:52:15 -0400

    Create permutations-v1.cpp

---
+++ 46/permutations-v1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    vector<vector<int>> permute(vector<int>& nums) {
+      int n = nums.size();
+      if (n == 1) {
+        return {nums};
+      }
+      vector<vector<int>> ans;
+      for (int i = 0; i < n; i++) {
+        vector<int> rest;
+        for (int j = 0; j < i; j++) {
+          rest.push_back(nums[j]);
+        }
+        for (int j = i + 1; j < n; j++) {
+          rest.push_back(nums[j]);
+        }
+        vector<vector<int>> prest = permute(rest);
+        for (auto p: prest) {
+          p.push_back(nums[i]);
+          ans.push_back(p);
+        }
+      }
+      return ans;
+    }
+};
