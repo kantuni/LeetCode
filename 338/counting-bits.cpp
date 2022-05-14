d161a8f12520ea19cb61487387ab1a0163efc510
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-05-14 12:18:22 -0400

    Update counting-bits.cpp

--- 338/counting-bits.cpp
+++ 338/counting-bits.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     vector<int> countBits(int n) {
       vector<int> ans;
-      for (int i = 0, j = 0; i <= n; i++, j++) {
+      for (int i = 0; i <= n; i++) {
         ans.push_back(__builtin_popcount(i));
       }
       return ans;
