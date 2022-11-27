589bae17362175693756df348d3206cebe8f516a
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-11-26 19:33:23 -0500

    Update guess-number-higher-or-lower.cpp

--- 374/guess-number-higher-or-lower.cpp
+++ 374/guess-number-higher-or-lower.cpp
@@ -1,8 +1,8 @@
 /** 
  * Forward declaration of guess API.
  * @param  num   your guess
- * @return 	     -1 if num is higher than the picked number
- *			         1 if num is lower than the picked number
+ * @return       -1 if num is higher than the picked number
+ *               1 if num is lower than the picked number
  *               0 otherwise
  * int guess(int num);
  */
