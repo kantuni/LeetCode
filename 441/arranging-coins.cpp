5b10ffa0d010e2d1ba2a641a69a073dce6d5d7f3
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-12-04 23:36:20 -0500

    Update arranging-coins.cpp

--- 441/arranging-coins.cpp
+++ 441/arranging-coins.cpp
@@ -2,7 +2,7 @@ class Solution {
   public:
     int arrangeCoins(int n) {
       int ans = 0;
-      for (int64_t i = 1; i * (i + 1) / 2 <= n; i++) {
+      for (long long i = 1; i * (i + 1) / 2 <= n; i++) {
         ans++;
       }
       return ans;
