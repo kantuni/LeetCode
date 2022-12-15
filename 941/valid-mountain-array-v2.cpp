404b51e99d2d7f7b39661be80ab6a6490e83962f
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2022-12-15 00:38:23 -0500

    Create valid-mountain-array-v2.cpp

---
+++ 941/valid-mountain-array-v2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    bool validMountainArray(vector<int>& arr) {
+      int n = arr.size();
+      // 2 pointers from left and right should meet
+      // at the same location, which can't be the beginning
+      // or the end.
+      int i = 0;
+      while (i + 1 < n and arr[i] < arr[i + 1]) {
+        i++;
+      }
+      int j = n - 1;
+      while (j > 0 and arr[j - 1] > arr[j]) {
+        j--;
+      }
+      return i > 0 and j < n - 1 and i == j;
+    }
+};
