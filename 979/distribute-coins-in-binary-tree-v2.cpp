842e307c050c24596c1b0f24ee6ba3fb4042a9ea
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2024-11-11 09:24:18 -0500

    Create distribute-coins-in-binary-tree-v2.cpp

---
+++ 979/distribute-coins-in-binary-tree-v2.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *   int val;
+ *   TreeNode *left;
+ *   TreeNode *right;
+ *   TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *   TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+  public:
+    int distributeCoins(TreeNode* root) {
+      auto [_, cost] = helper(root);
+      return cost;
+    }
+
+    pair<int, int> helper(TreeNode* root) {
+      if (root == nullptr) {
+        return {0, 0};
+      }
+      if (root->left and root->right) {
+        auto [vl, cl] = helper(root->left);
+        auto [vr, cr] = helper(root->right);
+        int nv = root->val + (vl - 1) + (vr - 1);
+        root->val = nv;
+        return {nv, cl + cr + abs(vl - 1) + abs(vr - 1)};
+      }
+      if (root->left) {
+        auto [vl, cl] = helper(root->left);
+        int nv = root->val + (vl - 1);
+        root->val = nv;
+        return {nv, cl + abs(vl - 1)};
+      }
+      if (root->right) {
+        auto [vr, cr] = helper(root->right);
+        int nv = root->val + (vr - 1);
+        root->val = nv;
+        return {nv, cr + abs(vr - 1)};
+      }
+      int nv = root->val;
+      root->val = 1;
+      return {nv, 0};
+    }
+};
